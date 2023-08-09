8ce01b07d8bf247f8ece8ee5d0d0357951f03dd7
CindyArbelo <140919757+CindyArbelo@users.noreply.github.com>
2023-08-08 21:20:39 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded",function () {
   const btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
+  
   alert("Hola! Soy el div");
 });
 });
-
 
 
